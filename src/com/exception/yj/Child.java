132b54cb0dda26d93bf4c3c22d07a9e26ea88710
Yeshwanth Jadhav <yeshwanth.jadhav@gmail.com>
2017-11-18 01:18:30 -0600

    first commit

---
+++ src/com/exception/yj/Child.java
@@ -0,0 +1,5 @@
+package com.exception.yj;
+
+public class Child extends Parent{
+	Child() throws Exception {}
+}
---
+++ src/com/exception/yj/Parent.java
@@ -0,0 +1,7 @@
+package com.exception.yj;
+
+import java.io.IOException;
+
+public class Parent {
+	Parent() throws IOException{}
+}
